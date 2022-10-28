341374718437f5e7393397d6405993e7769ef924
reda <reda.himmi@edu.uca.ma>
2022-10-28 23:54:52 +0100

    fix socket connection front

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 export const connectSocket = (token) => {
-  return io(process.env.REACT_APP_WS_URL, {
+  return io(process.env.REACT_APP_WS_URL || "ws://localhost:5000", {
     transports: ["websocket"],
     auth: { token },
   });
